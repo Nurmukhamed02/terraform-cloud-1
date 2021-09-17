31b8c32250f344f12176bf8adadfb5a2aaccd5ec
Nurmukhamed02 <85335166+Nurmukhamed02@users.noreply.github.com>
2021-09-17 16:44:46 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,37 +3,8 @@ provider "aws" {
 }
 
 #========================================================================================
-terraform {
-  backend "remote" {
-      hostname = "app.terraform.io"
-      organization = "ant-engineering"
-      
-    workspace {
-      name = "DEV"
-   }
- }
-}
-#=============================================================================================
-data "aws_ami" "latest_amazon" {
-  owners = ["amazon"]
-  most_recent = true
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
 
-
-
-
-resource "aws_instance" "amazon_instance" {
-    count = 2
     ami = data.aws_ami.latest_amazon.id
     instance_type = "t2.micro"
     
    
-    tags = {
-      Name = "My EC2"
-   
-   }
-}
